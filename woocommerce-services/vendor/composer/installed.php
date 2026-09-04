69b8b3ffc8de93208ad1cd98cc1a38e697baf4ac
Justin Gardner <justin@rhynorater.com>
2026-09-04 06:04:43 -0500

    3.6.14

--- woocommerce-services/vendor/composer/installed.php
+++ woocommerce-services/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-trunk',
         'version' => 'dev-trunk',
-        'reference' => '590070bfd320d3d9171a30e310b50b642b0aa010',
+        'reference' => 'b732771aeb7fd017058f213e4278e4ba91fe6310',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-trunk',
             'version' => 'dev-trunk',
-            'reference' => '590070bfd320d3d9171a30e310b50b642b0aa010',
+            'reference' => 'b732771aeb7fd017058f213e4278e4ba91fe6310',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
